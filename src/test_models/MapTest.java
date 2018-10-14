ef4c23294947b611ca93012692d87893ac8140be
hr-gith <hamideh.riazat@gmail.com>
2018-10-14 13:14:54 -0400

    added map test class

---
+++ src/test_models/MapTest.java
@@ -0,0 +1,15 @@
+package test_models;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class MapTest {
+
+	@Test
+	void AddContinentTest() {
+		
+		fail("Not yet implemented");
+	}
+
+}
