06ac9bc8895ab7ec182268466ec094998d3b5bb3
hr-gith <hamideh.riazat@gmail.com>
2018-10-19 16:29:53 -0400

    invalid map is created

--- src/utilities/Config.java
+++ src/utilities/Config.java
@@ -5,7 +5,7 @@ package utilities;
 public class Config {
 
 
-	public static String input_file = ".\\src\\map2.map";
+	public static String input_file = ".\\src\\map1.map";
 	public static String output_file = ".\\src\\map_out.map";
 
 
