6f7a60f4e8ca0d5a456f7109eccc0689b30169e5
e_narang <e_narang@LIBERIA.ENCS.concordia.ca>
2018-11-03 16:25:20 -0400

    creating class Playe_World_Domination_view

---
+++ src/views/Players_World_Domination_View.java
@@ -0,0 +1,19 @@
+package views;
+
+import java.util.Observable;
+
+public class Players_World_Domination_View extends View {
+
+	@Override
+	public void update(Observable arg0, Object arg1) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void Display(String text) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
