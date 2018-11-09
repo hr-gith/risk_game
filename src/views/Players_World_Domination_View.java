8ccdf5374d2c3a0b5dd1dd13c3c8f87dfdf8fd06
e_narang <e_narang@BELGIUM.ENCS.concordia.ca>
2018-11-08 20:29:31 -0500

    Changed on Players_domination_View

--- src/views/Players_World_Domination_View.java
+++ src/views/Players_World_Domination_View.java
@@ -1,8 +1,12 @@
 package views;
 
 import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Toolkit;
 import java.util.Observable;
 import javax.swing.*;
+import javax.swing.border.EmptyBorder;
+
 import models.Game_Model;
 
 /**
@@ -26,7 +30,7 @@ public class Players_World_Domination_View extends View {
 	 */
 	public Players_World_Domination_View() {
 		jPanel = new JPanel();
-		jLabel1 = new JLabel();
+		jLabel1 = new JLabel("<html>Case &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Item&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; CaseNum<br><font color=yellow>Party1<br>Party2</font></html>");
 		jLabel2 = new JLabel();
 		jLabel3 = new JLabel();
 	}
@@ -40,6 +44,8 @@ public class Players_World_Domination_View extends View {
 		this.continent_owner = ((Game_Model) obs).Continent_Owner();
 		this.percentage_of_world_owner = ((Game_Model) obs).Percentage_of_world_Owner();
 		Draw_Players_World_Domination_View_Window();
+		 Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+	//        setBounds(10,200,screenSize.width, screenSize.height);
 	}
 
 	/**
@@ -56,19 +62,35 @@ public class Players_World_Domination_View extends View {
 		jPanel.add(this);
 		jPanel.setVisible(true);
 		jPanel.setLayout(null);
+		 EmptyBorder border1 = new EmptyBorder(5, 20, 15, 18);
 
 		jPanel.setBackground(Color.yellow);
 
 		jLabel1.setText(percentage_of_world_owner);
-		jLabel1.setBounds(0, 0, 200, 50);
+		jLabel1.setBounds(10, 10, 200, 50);
+		jLabel1.setBackground(Color.GRAY);
+		 jLabel1.setSize(600, 100);
+		 jLabel1.setBorder(border1);
+         Dimension d = jLabel1.getPreferredSize();
+		  jLabel1.setOpaque(true);
 		jPanel.add(jLabel1);
 
 		jLabel2.setText(continent_owner);
-		jLabel2.setBounds(0, 20, 200, 50);
+		jLabel2.setBounds(10, 200, 200, 50);
+		jLabel2.setBackground(Color.orange);
+		 jLabel2.setSize(600, 100);
+		 jLabel2.setBorder(border1);
+         Dimension p = jLabel2.getPreferredSize();
+		  jLabel2.setOpaque(true);
 		jPanel.add(jLabel2);
 
 		jLabel3.setText(total_number_of_armies_Of_player);
-		jLabel3.setBounds(0, 40, 200, 50);
+		jLabel3.setBounds(10, 400, 200, 50);
+		jLabel3.setBackground(Color.cyan);
+		 jLabel3.setSize(600, 100);
+		 jLabel3.setBorder(border1);
+         Dimension l = jLabel3.getPreferredSize();
+		  jLabel3.setOpaque(true);
 		jPanel.add(jLabel3);
 
 	}
@@ -81,3 +103,4 @@ public class Players_World_Domination_View extends View {
 	}
 
 }
+
