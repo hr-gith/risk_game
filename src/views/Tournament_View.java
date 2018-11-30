535d27e994738f39369df9304dfa7077375fbcdc
l_noroo <l_noroo@LITHUANIA.ENCS.concordia.ca>
2018-11-30 18:00:44 -0500

    Tournament View completed

--- src/views/Tournament_View.java
+++ src/views/Tournament_View.java
@@ -5,7 +5,8 @@ import controllers.Tournament_Controller;
 import models.State_Player_Strategy;
 
 /**
- * this class is responsible for setup tournament and gathering information and pass to the controller
+ * this class is responsible for setup tournament and gathering information and
+ * pass to the controller
  *
  */
 public class Tournament_View {
@@ -15,6 +16,7 @@ public class Tournament_View {
 
 	/**
 	 * constructor for creating tournament controller object
+	 * 
 	 * @param controler
 	 */
 	public Tournament_View(Tournament_Controller controler) {
@@ -22,190 +24,168 @@ public class Tournament_View {
 	}
 
 	/**
-	 * Display menu for gathering information for tournament
-	 * @return integer result based on choice and pass it to the setup tournament function
-	 */
-	public int Display_Tournament_Menu() {
-		scanner = new Scanner(System.in);
-		System.out.println("\n==================================");
-		System.out.println("\n\t Tournament Mode");
-		System.out.println("\n==================================");
-		System.out.println("\n 1. For Entering Maps");
-		System.out.println("\n 2. For Entering Number of Games at each Map");
-		System.out.println("\n 3. For Entering Your Players Strategies");
-		System.out.println("\n 4. For Entering Number of Turn at each Game");
-		System.out.println("\n 5. Start tournament ");
-		System.out.println("\n 6. Exit ");
-		System.out.println("\n\n Please Enter Your Choice(1-6): ");
-
-		int result = Integer.valueOf(scanner.nextLine());
-		return result;
-	}
-
-	/**
-	 * this method is responsible for getting information and check correctness and pass to the tournament controller
+	 * this method is responsible for getting information and check correctness and
+	 * pass to the tournament controller
 	 */
 	public void Setup_Tournament() {
 
-		int choice;
 		String new_input = "n";
 		String new_strategy_input = "n";
 		boolean add_player_result = false;
-		
+
+		System.out.println("\n==================================");
+		System.out.println("\n\t Tournament Mode");
+
+		// Entering Map
+		System.out.println("\n==================================");
+		System.out.println("\n\t  MAP ");
+		System.out.println("\n==================================");
 
 		do {
-			choice = Display_Tournament_Menu();
+			scanner = new Scanner(System.in);
+			System.out.println("\n Enter Number of Map: ");
+			int map_number = Integer.valueOf(scanner.nextLine());
+			String map_name = "";
 
-			switch (choice) {
+			if (map_number < 1 || map_number > 5) {
+				System.out.println("\n\t Please enter between 1 to 5 ");
+				System.out.println("\n\t Try Again");
+				new_input = "y";
 
-			case 1:// Map Files between 1 to 5
-
-				do {
-					scanner = new Scanner(System.in);
-					System.out.println("\n Enter Number of Map: ");
-					int map_number = Integer.valueOf(scanner.nextLine());
-					String map_name = "";
-
-					if (map_number < 1 || map_number > 5) {
-						System.out.println("\n==================================");
-						System.out.println("\n\t Please enter between 1 to 5 ");
-						System.out.println("\n==================================");
-						System.out.println("\n\t Do you want to enter again(y,n):");
-						new_input = scanner.nextLine().toLowerCase();
-
-					} else {
-						for (int i = 0; i < map_number; i++) {
-							System.out.println("\nMap" + (i + 1) + ": Enter the Name: ");
-							map_name = scanner.nextLine();
-							tournament_controler.Add_Map(map_name);
-							new_input="n";
-						}
-					}
-
-				} while (new_input.equals("y"));
-
-				break;
-
-			case 2: // number of games to be played on each map between 1 to 5
-				do {
-					scanner = new Scanner(System.in);
-					System.out.println("\n Enter Number of Game on each Map: ");
-					int game_number = Integer.valueOf(scanner.nextLine());
-
-					if (game_number < 1 || game_number > 5) {
-						System.out.println("\n==================================");
-						System.out.println("\n\t Please enter between 1 to 5 ");
-						System.out.println("\n==================================");
-						System.out.println("\n\t Do you want to try again(y,n):");
-						new_input = scanner.nextLine().toLowerCase();
-
-					} else {
-						tournament_controler.nb_game = game_number;
-						new_input="n";
-					}
-
-				} while (new_input.equals("y"));
-
-				break;
-
-			case 3: // Player Strategies between 2 to 4
-				do {
-					scanner = new Scanner(System.in);
-					System.out.println("\n Enter Number of Players: ");
-					int strategies_number = Integer.valueOf(scanner.nextLine());
-
-					if (strategies_number < 2 || strategies_number > 4) {
-						System.out.println("\n==================================");
-						System.out.println("\n\t Please enter between 2 to 4 ");
-						System.out.println("\n==================================");
-						System.out.println("\n\t Do you want to try again(y,n):");
-						new_input = scanner.nextLine().toLowerCase();
-
-					} else {
-
-						
-							for (int i = 0; i < strategies_number; i++) {
-								do {
-									System.out.println("\n Enter player name: ");
-									String name = scanner.nextLine().toLowerCase();
-
-									System.out.println(
-											"\n Enter the player's Strategy(AGGRESSIVE, BENEVOLENT, RANDOM, CHEATER): ");
-									String strategy = scanner.nextLine().trim().toUpperCase();									
-									
-									/*if (!State_Player_Strategy.values().equals(strategy)) {
-										System.out.println("\n==================================");
-										System.out.println("\n\t Please enter correct strategy ");
-										System.out.println("\n==================================");
-										System.out.println("\n\t Do you want to try again(y,n):");
-										new_strategy_input = scanner.nextLine().toLowerCase();
-									}else {*/
-										add_player_result = tournament_controler.Add_Player(name,State_Player_Strategy.valueOf(strategy));
-										
-										if (add_player_result != true) {
-											
-											System.out.println("\n==================================");
-											System.out.println("\n\tError: Failed to add the player! ");
-											System.out.println("\n==================================");
-											System.out.println("\n\t Do you want to try again(y,n):");
-											new_strategy_input = scanner.nextLine().toLowerCase();
-											
-										}else {
-											System.out.println("\n==================================");
-											System.out.println("\n\t Player is added successfully ");
-											new_strategy_input ="n";
-										}
-									//}
-									
-								}while(new_strategy_input.equals("y"));	
-							}
-					}
-					
-				} while (new_input.equals("y"));
-
-				break;
-
-			case 4:// number of turn at each game 10 to 50
-				do {
-					scanner = new Scanner(System.in);
-					System.out.println("\n Enter Number of Turn at each Game: ");
-					int turn_number = Integer.valueOf(scanner.nextLine());
-
-					if (turn_number < 10 && turn_number > 50) {
-						System.out.println("\n==================================");
-						System.out.println("\n\t Please enter between 10 to 50 ");
-						System.out.println("\n==================================");
-						System.out.println("\n\t Do you want to try again(y,n):");
-						new_input = scanner.nextLine().toLowerCase();
-
-					} else {
-						tournament_controler.max_nb_turn = turn_number;
-						new_input ="n";
-					}
-
-				} while (new_input.equals("y"));
-
-				break;
-
-			case 5:// Start tournament
-				try {
-					if (!this.tournament_controler.Start())
-						System.out.println("Error: Tournament can not be started...");
-				} catch (InterruptedException e) {
-					e.printStackTrace();
+			} else {
+				for (int i = 0; i < map_number; i++) {
+					System.out.println("\nMap" + (i + 1) + ": Enter the Map Name: ");
+					map_name = scanner.nextLine();
+					tournament_controler.Add_Map(map_name);
+					new_input = "n";
 				}
-				break;
-
-			case 6:// Back to main menu
-
-				break;
-
-			default:
-				System.out.println("\n==================================");
-				System.out.println("\n\t Error! Please Enter Your Choice(1 to 6)");
-				break;
 			}
-		} while (choice != 6);
+
+		} while (new_input.equals("y"));
+
+		// number of games to be played on each map between 1 to 5
+		
+		System.out.println("\n==================================");
+		System.out.println("\n\t GAME");
+		System.out.println("\n==================================");
+
+		do {
+			scanner = new Scanner(System.in);
+			
+			System.out.println("\n Enter Number of Game on each Map: ");
+			int game_number = Integer.valueOf(scanner.nextLine());
+
+			if (game_number < 1 || game_number > 5) {
+				System.out.println("\n\t Please enter between 1 to 5 ");
+				System.out.println("\n\t Try Again");
+				new_input = "y";
+
+			} else {
+				tournament_controler.nb_game = game_number;
+				new_input = "n";
+			}
+
+		} while (new_input.equals("y"));
+
+		// Players Strategies
+		
+		System.out.println("\n==================================");
+		System.out.println("\n\t PLAYER ");
+		System.out.println("\n==================================");
+		
+		do {
+			scanner = new Scanner(System.in);
+			System.out.println("\n Please Enter Number of Players: ");
+			
+			int player_number = Integer.valueOf(scanner.nextLine());
+
+			if (player_number < 2 || player_number > 4) {
+				
+				System.out.println("\n\t Please enter between 2 to 4 ");
+				System.out.println("\n\t Try Again");
+				new_input = "y";
+
+			} else {
+
+				for (int i = 0; i < player_number; i++) {
+					
+					do {
+						System.out.println("\n Enter Name of Player: ");
+						String name = scanner.nextLine().toLowerCase();
+						
+						System.out.println("\n Enter player Strategy: "
+								+ "player's Strategies should be (AGGRESSIVE, BENEVOLENT, RANDOM, CHEATER):");
+						String strategy = scanner.nextLine().trim().toUpperCase();
+
+						if (!(strategy.equals("AGGRESSIVE") || strategy.equals("BENEVOLENT")
+								|| strategy.equals("RANDOM") || strategy.equals("CHEATER"))) {
+
+							System.out.println("\n\t Wrong Input Strategy !! ");
+							System.out.println("\n\t Try Again");
+							new_strategy_input = "y";
+
+						} else {
+
+							add_player_result = tournament_controler.Add_Player(name,
+									State_Player_Strategy.valueOf(strategy));
+
+							if (add_player_result != true) {
+
+								System.out.println("\n\t Error: Failed to add the player! ");
+								System.out.println("\n\t Try Again ");
+								new_strategy_input = "y";
+
+							} else {
+								System.out.println("\n==================================");
+								System.out.println("\n\t Player is added successfully ");
+								System.out.println("\n==================================");
+								new_strategy_input = "n";
+							}
+						}
+
+					} while (new_strategy_input.equals("y"));
+				}
+			}
+
+		} while (new_input.equals("y"));
+
+		// Enter Turn at each Game
+		
+		System.out.println("\n==================================");
+		System.out.println("\n\t PLAY");
+		System.out.println("\n==================================");
+
+		do {
+			scanner = new Scanner(System.in);
+			System.out.println("\n Please enter Nember of The Turn at Each Game: ");
+			int turn_number = Integer.valueOf(scanner.nextLine());
+
+			if (turn_number < 10 && turn_number > 50) {
+				System.out.println("\n\t Please enter between 10 to 50 ");
+				System.out.println("\n\t Try Again:");
+				new_input = "y";
+
+			} else {
+				tournament_controler.max_nb_turn = turn_number;
+				new_input = "n";
+			}
+
+		} while (new_input.equals("y"));
+
+		//Starting Tournament
+		
+		System.out.println("\n==================================");
+		System.out.println("\n\t Starting Tournament");
+		System.out.println("\n==================================");
+		
+		try {
+			if (!this.tournament_controler.Start())
+				System.out.println("\n\t Error: Tournament can not be started...");
+				System.out.println("\n==================================");
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+
 	}
 
 }
