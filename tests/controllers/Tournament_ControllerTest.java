582be9f06a2458eb919f34da678df250f9827426
hadimhm <hadimhm@gmail.com>
2018-12-01 14:11:18 -0500

    updated tournament tests

--- tests/controllers/Tournament_ControllerTest.java
+++ tests/controllers/Tournament_ControllerTest.java
@@ -1,6 +1,11 @@
 package controllers;
 import static org.junit.Assert.*;
 
+import java.util.AbstractMap;
+import java.util.ArrayList;
+
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 /**
@@ -8,6 +13,36 @@ import org.junit.Test;
  *
  */
 public class Tournament_ControllerTest {
+	
+	String map_name = "map3";
+	Tournament_Controller TC;
+	
+	/**
+	 * this method runs before other tests to set up the environment
+	 */
+	@Before public void setUp_Test(){
+		TC = new Tournament_Controller();		
+		}
+
+	/**
+	 * this method test if the tournament map has been correctly added
+	 */
+	@Test
+	public void Tournament_Add_Map_Test() {
+		
+		Assert.assertTrue(TC.Add_Map(map_name));
+	}
+	
+	
+	/**
+	 * this method test if the tournament view has been correctly created
+	 */
+	@Test
+	public void Tournament_view_Test() {
+		
+		Assert.assertNotNull(TC.tournament_view);
+	}
+	
 	
 	
 
